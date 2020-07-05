c94ae8896396d679f8ae8736252211d63613858a
YA <YA-androidapp@users.noreply.ithub.com>
2020-07-05 10:36:58 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import fourletterphat
 import requests
+import time
 
 
 def monitor(url):
@@ -8,7 +9,18 @@ def monitor(url):
     status_code = response.status_code
 
     fourletterphat.clear()
-    fourletterphat.scroll_print(str(status_code) + ' ' + url, 0.1 if (200 == status_code) else 1 )
+
+    fourletterphat.print_str(str(status_code))
+    fourletterphat.show()
+    time.sleep(5)
+
+    fourletterphat.scroll_print(str(status_code) + ' ' + url + ' ' + str(status_code), 0.1 if (200 == status_code) else 1 )
+
+    fourletterphat.print_number_str(str(status_code))
+    fourletterphat.show()
+    time.sleep(5)
+
+    fourletterphat.clear()
 
 with open('urls.txt') as f:
     line = f.readline()
